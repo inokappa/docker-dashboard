64e2b8b466050e1c0a5210b9a688a6be2f2f75dc
root <root@9f2f92e44f69.(none)>
2014-04-06 06:23:26 -0400

    modified.

--- jobs/docker.rb
+++ jobs/docker.rb
@@ -1,13 +1,28 @@
 require 'docker'
-docker_list = Hash.new({ value: 0 })
-
+run_docker_list = Hash.new({ value: 0 })
+all_docker_list = Hash.new({ value: 0 })
+#
 SCHEDULER.every '10s' do 
   Docker.url='http://172.17.42.1:4243/'
-  cons = Docker::Container.all(:running => true)
-  cons.each do |con|
-    $docker_id = con.id[0,12]
-    $docker_name = con.info.fetch("Names")
-    docker_list[$docker_id] = { label: $docker_id, value: $docker_name }
-    send_event('containers', { items: docker_list.values })
+  #
+  cons_run = Docker::Container.all(:running => true)
+  cons_all = Docker::Container.all(:all => true)
+  #
+  cons_run.each do |con|
+    docker_id = con.id[0,12]
+    docker_name = con.info.fetch("Names")
+    run_docker_list[docker_id] = { label: docker_id, value: docker_name }
+    send_event('containers', { items: run_docker_list.values })
   end
+  #
+  cons_all.each do |con|
+    docker_id = con.id[0,12]
+    docker_name = con.info.fetch("Names")
+    all_docker_list[docker_id] = { label: docker_id, value: docker_name }
+    send_event('allcontainers', { items: all_docker_list.values })
+  end
+  #
+  count = (cons_run.size.to_f / cons_all.size.to_f) * 100
+  send_event('countmater', { value: count.round } )
+  send_event('count', { current: cons_run.size } )
 end
