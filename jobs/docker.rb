62fae2f2376dabad79b0fbdc562d0f3aa92b2814
root <root@ip-192-168-0-167.ec2.internal>
2014-04-09 22:13:05 +0000

    modified.

--- jobs/docker.rb
+++ jobs/docker.rb
@@ -4,23 +4,18 @@ all_docker_list = Hash.new({ value: 0 })
 #
 SCHEDULER.every '10s' do 
   # Please Change Docker API ENDPOINT
-  Docker.url='http://172.17.42.1:4243/'
+  Docker.url='http://127.0.0.1:4243/'
   #
   cons_run = Docker::Container.all(:running => true)
   cons_all = Docker::Container.all(:all => true)
+  docker_ver = Docker.version
+  docker_info = Docker.info
   #
   cons_run.each do |con|
     docker_id = con.id[0,12]
-    docker_name = con.info.fetch("Names")
-    run_docker_list[docker_id] = { label: docker_id, value: docker_name }
+    docker_image = con.info.fetch("Image")
+    run_docker_list[docker_id] = { label: docker_id, value: docker_image }
     send_event('containers', { items: run_docker_list.values })
-  end
-  #
-  cons_all.each do |con|
-    docker_id = con.id[0,12]
-    docker_name = con.info.fetch("Names")
-    all_docker_list[docker_id] = { label: docker_id, value: docker_name }
-    send_event('allcontainers', { items: all_docker_list.values })
   end
   #
   count = (cons_run.size.to_f / cons_all.size.to_f) * 100
@@ -31,5 +26,14 @@ SCHEDULER.every '10s' do
   mem_f = %x[ cat /proc/meminfo | grep MemFree: | awk '{print $2}' ]
   mem_used = ( mem_f.to_f / mem_t.to_f) * 100
   send_event('memmater', { value: mem_used.round } )
-
+  #
+  cpu_i = %x[ top -n 1 -b | grep Cpu | awk '{print $5}' | sed 's/\%id,//g' ]
+  cpu_u = 100 - cpu_i.to_f
+  send_event('cpumater', { value: cpu_u.round } )
+  #
+  hostname = %x[ hostname -s]
+  send_event('docker', { text: hostname } )
+  send_event('docker', { moreinfo: docker_ver["Version"] } )
+  send_event('allcontainers', { current: docker_info["Containers"] } )
+  p docker_info["Containers"]
 end
